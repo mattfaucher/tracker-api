198b2a0f31190b9cd20bed6533286f8f78be9c56
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2021-07-21 16:18:11 -0400

    removed localhost db

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  //const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  const url = process.env.DB_URL;
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
